3013ece137506dc639338cd3fcb8b6598a41a925
strsev <evaevdokimova19@mail.ru>
2026-04-29 11:45:16 +0500

    quatro commit

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    use HasFactory;
+    use Sluggable;
+    public function posts()
+    {
+       return $this->hasMany(Post::class);
+    }
+
+    public function sluggable():array
+    {
+        return [
+                'slug' => [
+                    'source' => 'title'
+                ]
+            ];
+    }
 }
+
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,29 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    use Sluggable;
+
+    public function tags()
+    {
+        return $this-> belongsTomany(Tag::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function sluggable():array {
+        return [
+                'slug' => [
+                    'source' => 'title'
+                ]
+            ];
+    }
 }
