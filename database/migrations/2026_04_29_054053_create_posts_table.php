d9d3a15cd17b3160064b4598d49db9b15c417d72
strsev <evaevdokimova19@mail.ru>
2026-04-29 11:17:26 +0500

    three commit

---
+++ database/migrations/2026_04_29_054053_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->text('content');
+            $table->integer('category_id')->unsigned();
+            $table->integer('views')->unsigned()->default(0);
+            $table->string('thumbnail')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
